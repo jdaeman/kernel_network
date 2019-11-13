a63315a8b0cb7da8a0dbef164dceeecd601dc157
jdaeman <xordn25@naver.com>
2019-11-13 21:02:50 +0900

    spoof.c

--- test.c
+++ test.c
@@ -4,10 +4,15 @@
 #include <linux/fs.h>
 #include <net/ip_fib.h>
 
+#include <linux/net.h>
+#include <linux/inet.h>
+
+#include <linux/kthread.h>
+#include <linux/delay.h>
 
 static int test1(void)
 {
-	struct flowi4 f4;
+	/*struct flowi4 f4;
 	struct fib_table * ft;
 	int ret = fib_lookup(&init_net, &f4, &res, 0);
 	
@@ -21,7 +26,7 @@ static int test1(void)
 		return -1;
 	}
 
-	printk("ft is not null\n");
+	printk("ft is not null\n");*/
 	return 0;
 }
 
@@ -46,9 +51,73 @@ static int test2(void)
 	return -1;
 }
 
+static int test3(void)
+{
+	struct socket * socket = NULL;
+	int err = sock_create(PF_INET, SOCK_DGRAM, 0, &socket);
+	extern struct proto ping_prot;
+
+	struct sockaddr_in sin;
+	struct msghdr msg;
+	struct iovec iov;
+	char buf[] = "Hello";
+	int len = 5;
+
+	if (err < 0)
+		printk("err code: %d\n", err);
+	else
+		printk("socket is create\n");
+
+	if (!socket)
+		return -1;
+
+	memset(&msg, 0, sizeof(msg));
+	sin.sin_addr.s_addr = in_aton("192.168.0.1");
+	msg.msg_name = &sin;
+	iov.iov_base = buf;
+	iov.iov_len = len;
+	
+	iov_iter_init(&msg.msg_iter, WRITE, &iov, 1, len);
+
+	printk("ret: %d\n", ping_prot.sendmsg(socket->sk, &msg, len));
+
+
+	if (socket->ops)
+		printk("ops is exist\n");
+	else
+		printk("ops is not exist\n");
+
+	sock_release(socket);
+
+	return -1;
+}
+
+int tt(void * ptr)
+{
+	printk("TT\n");
+	msleep(2);
+	return 100;
+}
+
+static int test4(void)
+{
+	struct task_struct * ts = NULL;
+	ts = kthread_run(tt, NULL, "TTT");
+	if (!ts)
+		return -1;
+
+	//msleep(5);
+	printk("check tt task\n");
+	printk("state: %ld\n", ts->state);
+	printk("exit_state: %d, exit_code: %d\n", ts->exit_state, ts->exit_code);
+
+	return -1;
+	
+}
+
 int test_init(void)
 {
-	
+	test4();
 	return -1;
 }
 
